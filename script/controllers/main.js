9137335943b46e0d3a1a137a800d3f6e7710a378
UtkinG <utking@mail.ru>
2017-05-10 07:07:16 +1000

    Add a filter for a more flexible search withing the fetched stories

--- script/controllers/main.js
+++ script/controllers/main.js
@@ -1,23 +1,32 @@
 (function() {
-  var app = angular.module('HackerNews');
-  app.controller('MainCtrl', ['$scope', '$q', 'StorageService',
-    function($scope, $q, StorageService) {
+  angular.module('HackerNews')
+  .constant('API_BASE_URL', 'https://hacker-news.firebaseio.com/v0')
+  .controller('MainCtrl', ['$scope', '$q', 
+    'StorageService', 'subjectFilter', 'API_BASE_URL',
+    function($scope, $q, StorageService, subjectFilter, API_BASE_URL) {
     $scope.news = [];
     $scope.curStep = 'Fetching the list...';
+    $scope.filters = [
+      { title: 'All' },
+      { title: 'General', value: 'js,script,node,css,style' },
+      { title: 'JavaScript', value: 'js,script,node' },
+      { title: 'Css', value: 'css,style' },
+      { title: 'Angular', value: 'angular' }
+    ];
+    $scope.curFilter = $scope.filters[1].value;
 
-    const apiBaseUrl = 'https://hacker-news.firebaseio.com/v0';
     var newsUrls = [];
     var deferred = $q.defer();
     var storage = StorageService.getStorage('hackernews');
 
-    fetch(apiBaseUrl+'/topstories.json')
+    fetch(API_BASE_URL+'/topstories.json')
       .then(function(x) { return x.json(); })
       .then(function(x) {
         x.forEach(function(a, n) {
           var prevItem = storage.get(''+a);
           if (prevItem === null || prevItem === undefined) {
             newsUrls.push(
-              fetch(apiBaseUrl+'/item/'+a+'.json')
+              fetch(API_BASE_URL+'/item/'+a+'.json')
               .then(function(i) {
                 $scope.curStep = 'Fetching items... ' + n;
                 return i.json();
@@ -26,20 +35,14 @@
                 $scope.error = err;
               }));
           } else {
-            if (!prevItem.nonJs)
-              $scope.news.push(prevItem);
+            $scope.news.push(prevItem);
           }
         });
         $q.all(newsUrls).then(function(results){
           $scope.curStep = 'Filtering items...';
 
           results.forEach(function(i) {
-		        if (i.url && (/js/i.test(i.title) || /script/i.test(i.title))) {
-              storage.set(''+i.id, {type: i.type, title: i.title, url: i.url});
-              $scope.news.push({type: i.type, title: i.title, url: i.url});
-            } else {
-              storage.set(''+i.id, {nonJs: true, type: i.type, title: i.title, url: i.url});
-            }
+            $scope.news.push({type: i.type, title: i.title, url: i.url});
           });
           $scope.curStep = null;
         });
---
+++ script/filters/SubjectFilter.js
@@ -0,0 +1,22 @@
+(function () {
+  angular.module('HackerNews')
+  .filter('subject',[function () {
+    return function(array, subjArrayStr) {
+      if (!subjArrayStr)
+        return array;
+      var reArr = [];
+      subjArrayStr.split(',').forEach(function(i) {
+        reArr.push(new RegExp(i,'i'));
+      });
+      return array.filter(function(i) {
+        var result = false;
+        reArr.forEach(function(r) {
+          if (r.test(i.title)) {
+            result = true;
+          }
+        });
+        return result;
+      });
+    }
+  }]);
+})();
