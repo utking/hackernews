00fc12a8490ad2e8abd46adf9c3c6e9b13a77667
UtkinG <utking@mail.ru>
2017-05-09 16:04:41 +1000

    Add a caching mechanism (based on localStorage) for already fetched items

--- script/controllers/main.js
+++ script/controllers/main.js
@@ -1,33 +1,44 @@
 (function() {
   var app = angular.module('HackerNews');
-  app.controller('MainCtrl', ['$scope', '$q', function($scope, $q) {
+  app.controller('MainCtrl', ['$scope', '$q', 'StorageService',
+    function($scope, $q, StorageService) {
     $scope.news = [];
     $scope.curStep = 'Fetching the list...';
 
     const apiBaseUrl = 'https://hacker-news.firebaseio.com/v0';
     var newsUrls = [];
     var deferred = $q.defer();
+    var storage = StorageService.getStorage('hackernews');
 
     fetch(apiBaseUrl+'/topstories.json')
       .then(function(x) { return x.json(); })
       .then(function(x) {
         x.forEach(function(a, n) {
-          newsUrls.push(
-            fetch(apiBaseUrl+'/item/'+a+'.json')
-            .then(function(i) {
-              $scope.curStep = 'Fetching items... ' + n;
-              return i.json();
-            })
-            .catch(function(err) {
-              $scope.error = err;
-            }));
+          var prevItem = storage.get(''+a);
+          if (prevItem === null || prevItem === undefined) {
+            newsUrls.push(
+              fetch(apiBaseUrl+'/item/'+a+'.json')
+              .then(function(i) {
+                $scope.curStep = 'Fetching items... ' + n;
+                return i.json();
+              })
+              .catch(function(err) {
+                $scope.error = err;
+              }));
+          } else {
+            if (!prevItem.nonJs)
+              $scope.news.push(prevItem);
+          }
         });
         $q.all(newsUrls).then(function(results){
           $scope.curStep = 'Filtering items...';
 
           results.forEach(function(i) {
 		        if (i.url && (/js/i.test(i.title) || /script/i.test(i.title))) {
+              storage.set(''+i.id, {type: i.type, title: i.title, url: i.url});
               $scope.news.push({type: i.type, title: i.title, url: i.url});
+            } else {
+              storage.set(''+i.id, {nonJs: true, type: i.type, title: i.title, url: i.url});
             }
           });
           $scope.curStep = null;
---
+++ script/services/storage.js
@@ -0,0 +1,142 @@
+(function(module) {
+  angular.module('HackerNews')
+  .factory('StorageService', [function() {
+
+    /**
+     * LocalStorage class allowing to store any types including
+     * arrays and objects. Uses localStorage when it's possible
+     * or internal storage if not
+     * @param value
+     * @returns {LocalStorage}
+     * @constructor
+     */
+    var LocalStorage = function(value) {
+
+        if (false === this instanceof LocalStorage) {
+            return new LocalStorage(value);
+        }
+
+        if (!value || !value.charAt) {
+            throw new Error('You should specify the basket name');
+        } else {
+            this.basket = value;
+        }
+        try {
+            // try to use the built-in localStorage
+            this.localStorage = window.localStorage;
+        } catch (e) {
+            // or use an object-based replacement
+            var _store = {};
+
+            var _getItem = function (prop, defVal) {
+                if (_store[prop]) {
+                    return _store[prop];
+                }
+                return defVal !== undefined ? defVal : null;
+            };
+
+            var _setItem = function (prop, val) {
+                _store[prop] = val;
+            };
+
+            var _removeItem = function (prop) {
+                _store[prop] = undefined;
+            };
+
+            var _clean = function () {
+                _store = {};
+            };
+
+            this.localStorage = {
+                getItem: _getItem,
+                setItem: _setItem,
+                removeItem: _removeItem,
+                clean: _clean
+            };
+        }
+        return this;
+    };
+
+    /**
+     * Save the 'val' into the storage as a variable
+     * with the 'prop' name
+     * @param prop
+     * @param val
+     */
+    LocalStorage.prototype.set = function (prop, val) {
+        if (!this.basket) {
+            throw new Error('You should specify the basket name first');
+        }
+        if (!prop || !prop.charAt) {
+            throw new Error('You should specify the property name');
+        }
+        this.localStorage.setItem(this.basket+'.'+prop, JSON.stringify(val));
+        return this;
+    };
+
+    /**
+     * Get a value of the variable 'prop'. Return
+     * defVal if there is no such 'prop' in the storage
+     * @param prop
+     * @param defVal
+     * @returns {*}
+     */
+    LocalStorage.prototype.get = function (prop, defVal) {
+        if (!this.basket) {
+            throw new Error('You should specify the basket name first');
+        }
+        if (!prop || !prop.charAt) {
+            throw new Error('You should specify the property name');
+        }
+
+        var val = this.localStorage.getItem(this.basket+'.'+prop);
+        if (val === null) {
+            return defVal;
+        }
+        return JSON.parse(val);
+    };
+
+    /**
+     * Remove the item with the 'prop' name
+     * @param prop
+     */
+    LocalStorage.prototype.remove = function (prop) {
+        if (!this.basket) {
+            throw new Error('You should specify the basket name first');
+        }
+        if (!prop || !prop.charAt) {
+            throw new Error('You should specify the property name');
+        }
+        this.localStorage.removeItem(this.basket+'.'+prop);
+        return this;
+    };
+
+    /**
+     * Clean all the variables whose names start with the basket name
+     */
+    LocalStorage.prototype.clean = function () {
+        var r = new RegExp('^'+this.basket+'\\.');
+        try {
+            // determine what storate to use
+            for (var i in window.localStorage) {
+                if (r.test(i)) {
+                    this.localStorage.removeItem(i);
+                }
+            }
+        } catch (e) {
+            this.localStorage.clean();
+        }
+        return this;
+    };
+
+    /**
+     * Public interface
+     */
+    return {
+      getStorage: function (basketName) {
+        return new LocalStorage(basketName);
+      }
+    };
+  }]);
+})(this);
+
