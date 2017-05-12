b31a8dd3d33465dffe69208a78060dc63dc0f934
UtkinG <utking@mail.ru>
2017-05-12 18:05:54 +1000

    Add a refresh button. Add the API filter

--- script/controllers/main.js
+++ script/controllers/main.js
@@ -5,15 +5,16 @@
     'StorageService', 'subjectFilter', 'API_BASE_URL',
     function($scope, $q, $filter, StorageService, subjectFilter, API_BASE_URL) {
     $scope.news = [];
-    $scope.curStep = 'Fetching the list...';
     $scope.filters = [
       { title: 'All' },
       { title: 'General', value: '\bjs\b,(ecma|java).*script,node.?js,css,style' },
       { title: 'JavaScript', value: '\bjs\b,(ecma|java).*script,node.?js' },
+      { title: 'API', value: '\bapi\b' },
       { title: 'Css', value: 'css,style' },
       { title: 'Angular', value: 'angular' }
     ];
     $scope.curFilter = $scope.filters[1].value;
+    $scope.customeFilter = $scope.filters[0];
 
     $scope.filterNews = function() {
       return $filter('subject')($scope.news, $scope.curFilter).length;
@@ -23,38 +24,47 @@
     var deferred = $q.defer();
     var storage = StorageService.getStorage('hackernews');
 
-    fetch(API_BASE_URL+'/topstories.json')
-      .then(function(x) { return x.json(); })
-      .then(function(x) {
-        x.forEach(function(a, n) {
-          var prevItem = storage.get(''+a);
-          if (prevItem === null || prevItem === undefined) {
-            newsUrls.push(
-              fetch(API_BASE_URL+'/item/'+a+'.json')
-              .then(function(i) {
-                $scope.curStep = 'Fetching items... ' + n;
-                return i.json();
-              })
-              .catch(function(err) {
-                $scope.error = err;
-              }));
-          } else {
-            if (prevItem.url)
-              $scope.news.push(prevItem);
-          }
-        });
-        $q.all(newsUrls).then(function(results){
-          $scope.curStep = 'Filtering items...';
-
-          results.forEach(function(i) {
-            storage.set(''+i.id, {type: i.type, title: i.title, url: i.url, time: i.time});
-            if (i.url) {
-              $scope.news.push({type: i.type, title: i.title, url: i.url, time: i.time});
+    $scope.refreshItems = function(filter) {
+      if (filter)
+        $scope.curFilter = filter;
+      $scope.news = [];
+      newsUrls = [];
+      $scope.curStep = 'Fetching the list...';
+      fetch(API_BASE_URL+'/topstories.json')
+        .then(function(x) { return x.json(); })
+        .then(function(x) {
+          x.forEach(function(a, n) {
+            var prevItem = storage.get(''+a);
+            if (prevItem === null || prevItem === undefined) {
+              newsUrls.push(
+                fetch(API_BASE_URL+'/item/'+a+'.json')
+                .then(function(i) {
+                  $scope.curStep = 'Fetching items... ' + n;
+                  return i.json();
+                })
+                .catch(function(err) {
+                  $scope.error = err;
+                }));
+            } else {
+              if (prevItem.url)
+                $scope.news.push(prevItem);
             }
           });
-          $scope.curStep = null;
+          $q.all(newsUrls).then(function(results){
+            $scope.curStep = 'Filtering items...';
+
+            results.forEach(function(i) {
+              storage.set(''+i.id, {type: i.type, title: i.title, url: i.url, time: i.time});
+              if (i.url) {
+                $scope.news.push({type: i.type, title: i.title, url: i.url, time: i.time});
+              }
+            });
+            $scope.curStep = null;
+          });
         });
-      });
+    };
+
+    $scope.refreshItems();
 
   }]);
 })();
